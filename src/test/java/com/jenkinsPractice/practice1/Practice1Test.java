bec10711ee19f0f28b9e589c24fb0fa3ba3c9c86
lishikavarsha <106369162+lishikavarsha@users.noreply.github.com>
2022-05-31 14:22:52 +0530

    Update Practice1Test.java

--- src/test/java/com/jenkinsPractice/practice1/Practice1Test.java
+++ src/test/java/com/jenkinsPractice/practice1/Practice1Test.java
@@ -5,5 +5,6 @@ public class Practice1Test {
 	public static void main(String[] args) {
 		System.out.println("practice1");
 		System.out.println("practice1");
+		System.out.println("practice1");
 	}
 }
