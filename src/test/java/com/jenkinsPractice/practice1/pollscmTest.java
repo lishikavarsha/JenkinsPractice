d11db6485063e792e3e98f5dd0666c7226a3bdde
Admin <Admin@DESKTOP-K63190B>
2022-05-31 12:37:20 +0530

    this is my second push

---
+++ src/test/java/com/jenkinsPractice/practice1/pollscmTest.java
@@ -0,0 +1,13 @@
+package com.jenkinsPractice.practice1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class pollscmTest {
+	
+	@Test
+	public void ty(){
+	Reporter.log("pollscm",true);	
+	}
+	
+}
