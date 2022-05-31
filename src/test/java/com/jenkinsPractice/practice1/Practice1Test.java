4d7737ca5133a1b7065f6c39d6de393debc58e96
Admin <Admin@DESKTOP-K63190B>
2022-05-31 10:24:11 +0530

    this is my first push

---
+++ src/test/java/com/jenkinsPractice/practice1/Practice1Test.java
@@ -0,0 +1,8 @@
+package com.jenkinsPractice.practice1;
+
+public class Practice1Test {
+
+	public static void main(String[] args) {
+		System.out.println("practice1");
+	}
+}
---
+++ src/test/java/com/jenkinsPractice/practice1/Practice2Test.java
@@ -0,0 +1,10 @@
+package com.jenkinsPractice.practice1;
+
+public class Practice2Test {
+
+	public static void main(String[] args) {
+		System.out.println("practice2");
+
+	}
+
+}
